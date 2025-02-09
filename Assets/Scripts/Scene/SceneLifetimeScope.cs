afdd0ea8ec0e0c7152cb981a434bc5f90a578ceb
re5nardo <re5nardo@naver.com>
2025-02-09 14:14:05 +0900

    SceneLifetimeScope에 DI 로직 추가

--- Assets/Scripts/Scene/SceneLifetimeScope.cs
+++ Assets/Scripts/Scene/SceneLifetimeScope.cs
@@ -1,7 +1,28 @@
-using VContainer;
+using GameFramework;
+using UnityEngine.SceneManagement;
 using VContainer.Unity;
 
 namespace LOP
 {
-    public class SceneLifetimeScope : LifetimeScope { }
+    public class SceneLifetimeScope : LifetimeScope
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            var activeScene = SceneManager.GetActiveScene();
+
+            var DIGameObjects = activeScene.FindGameObjectsWithAttribute<DIGameObjectAttribute>();
+            foreach (var DIGameObject in DIGameObjects.OrEmpty())
+            {
+                Container.InjectGameObject(DIGameObject);
+            }
+
+            var DIMonoBehaviours = activeScene.FindComponentsWithAttribute<DIMonoBehaviourAttribute>();
+            foreach (var DIMonoBehaviour in DIMonoBehaviours.OrEmpty())
+            {
+                Container.Inject(DIMonoBehaviour);
+            }
+        }
+    }
 }

7146b4df11852b0ae2ea80e542d4ee30b40d1ab3
re5nardo <re5nardo@naver.com>
2025-04-02 23:24:12 +0900

    IDataContext , IDataContextManager 기본 적용

--- Assets/Scripts/Data/RoomDataContext.cs
+++ Assets/Scripts/Data/RoomDataContext.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameFramework;
+using System;
 
 namespace LOP
 {
     public partial class RoomDataContext : IDataContext
     {
+        public Type[] subscribedTypes => new Type[] { };
+
         public RoomDto room;
         public MatchDto match;
 
-        public RoomDataContext() { }
+        public void UpdateData<T>(T data)
+        {
+        }
 
         public void Clear()
         {
--- Assets/Scripts/RootLifetimeScope.cs
+++ Assets/Scripts/RootLifetimeScope.cs
@@ -9,6 +9,8 @@ namespace LOP
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<IMasterDataManager, LOPMasterDataManager>(Lifetime.Singleton);
+            builder.Register<IDataContextManager, LOPDataContextManager>(Lifetime.Singleton);
+            builder.Register<IDataContext, RoomDataContext>(Lifetime.Transient);
         }
     }
 }

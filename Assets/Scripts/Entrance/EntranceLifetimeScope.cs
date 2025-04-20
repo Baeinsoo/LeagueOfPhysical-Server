2a2dc4a6b641be818541b9ee94424ce3bd970356
re5nardo <re5nardo@naver.com>
2025-04-20 13:27:55 +0900

    base.Configure 호출하도록 수정

--- Assets/Scripts/Entrance/EntranceLifetimeScope.cs
+++ Assets/Scripts/Entrance/EntranceLifetimeScope.cs
@@ -7,6 +7,8 @@ namespace LOP
     {
         protected override void Configure(IContainerBuilder builder)
         {
+            base.Configure(builder);
+
             builder.Register<IEntranceComponent, ConfigureRoomComponent>(Lifetime.Transient);
             builder.Register<IEntranceComponent, LoadMasterDataComponent>(Lifetime.Transient);
         }

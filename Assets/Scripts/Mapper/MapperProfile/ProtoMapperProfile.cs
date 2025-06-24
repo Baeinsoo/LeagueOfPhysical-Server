542fc0508cc8cab159d53b5b60841cf51a8ac3d1
re5nardo <re5nardo@naver.com>
2025-06-24 23:43:53 +0900

    ProtoMapper 추가

--- Assets/Scripts/Mapper/MapperProfile/ProtoMapperProfile.cs
+++ Assets/Scripts/Mapper/MapperProfile/ProtoMapperProfile.cs
@@ -9,6 +9,9 @@ namespace LOP
         {
             CreateMap<ProtoVector3, Vector3>();
             CreateMap<Vector3, ProtoVector3>();
+
+            CreateMap<ProtoTransform, EntityTransform>();
+            CreateMap<EntityTransform, ProtoTransform>();
         }
     }
 }

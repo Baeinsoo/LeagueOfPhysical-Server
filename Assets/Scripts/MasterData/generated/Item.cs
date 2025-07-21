b00f0167b529debd81f0a9ce56ecd4fe5dad81a4
re5nardo <re5nardo@naver.com>
2025-07-22 00:05:20 +0900

    Item 데이터 추가

---
+++ Assets/Scripts/MasterData/generated/Item.cs
@@ -0,0 +1,11 @@
+using GameFramework;
+
+namespace LOP.MasterData
+{
+    public sealed class Item : IMasterData
+    {
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+        public string SkinCode { get; private set; }
+    }
+}

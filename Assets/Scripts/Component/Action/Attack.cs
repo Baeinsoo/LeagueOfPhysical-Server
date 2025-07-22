85958316e8a496ad64534043f34b5b0586031754
re5nardo <re5nardo@naver.com>
2025-07-22 23:31:47 +0900

    임시 어택 구현

--- Assets/Scripts/Component/Action/Attack.cs
+++ Assets/Scripts/Component/Action/Attack.cs
@@ -1,11 +1,69 @@
+using GameFramework;
+using UnityEngine;
 
 namespace LOP
 {
     public class Attack : Action
     {
+        private bool hasHit = false;
+        private float hitTime = 0.01f;
+        private float range = 2;
+        private float angle = 60;
+
         protected override void OnActionUpdate()
         {
+            if (elapsedTime >= hitTime && !hasHit)
+            {
+                PerformHit();
+                hasHit = true;
+            }
+        }
 
+        protected override void OnActionEnd()
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            hasHit = false;
+        }
+
+        private void PerformHit()
+        {
+            LayerMask layerMask = LayerMask.GetMask("Default");
+
+            Collider[] hits = Physics.OverlapSphere(entity.position, range, layerMask);
+            foreach (var hit in hits)
+            {
+                if (IsInAttackSector(entity, hit.transform.position, range, angle))
+                {
+                    if (hit.transform.name == "Plane")
+                    {
+                        continue;
+                    }
+
+                    ICombatSystem combatSystem = SceneLifetimeScope.Resolve<ICombatSystem>();
+                    combatSystem.Attack(entity, hit.transform.parent.parent.GetComponentInChildren<LOPEntity>());
+                }
+            }
+        }
+
+        bool IsInAttackSector(LOPEntity attacker, Vector3 targetPosition, float range, float angle)
+        {
+            Vector3 toTarget = targetPosition - attacker.position;
+            float distance = toTarget.magnitude;
+
+            if (distance > range)
+            {
+                return false;
+            }
+
+            Vector3 forward = Quaternion.Euler(attacker.rotation) * Vector3.forward;
+            float dot = Vector3.Dot(forward.normalized, toTarget.normalized);
+            float targetAngle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+
+            return targetAngle <= (angle * 0.5f);
         }
     }
 }

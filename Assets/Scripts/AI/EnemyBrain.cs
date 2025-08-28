5ebff0125ff23705bb4a9dd0eed58d6f48aaf013
re5nardo <re5nardo@naver.com>
2025-08-28 21:56:22 +0900

    Enemy가 근처 플레이어 타겟하도록 수정

--- Assets/Scripts/AI/EnemyBrain.cs
+++ Assets/Scripts/AI/EnemyBrain.cs
@@ -1,5 +1,6 @@
 using GameFramework;
 using System;
+using System.Linq;
 using UnityEngine;
 
 namespace LOP
@@ -16,49 +17,47 @@ namespace LOP
         public void Think(LOPEntity entity, double deltaTime)
         {
             //  Find the player
-            LOPEntity target = null;
             var entities = GameEngine.current.entityManager.GetEntities<LOPEntity>();
-            foreach (var otherEntity in entities)
+            LOPEntity target = entities
+                .Where(e => e.HasEntityComponent<PlayerComponent>())
+                .Where(e => (e.position - entity.position).magnitude <= 10)
+                .OrderBy(e => (e.position - entity.position).sqrMagnitude)
+                .FirstOrDefault();
+
+            if (target == null)
             {
-                if (otherEntity.HasEntityComponent<PlayerComponent>())
-                {
-                    target = otherEntity;
-                    break;
-                }
+                return;
             }
 
-            Vector3 direction = target.position -entity.position;
-            
-            if (direction.magnitude < 10f)
-            {
-                // Rotate
-                float myFloat = 0;
-                var angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-                var smooth = Mathf.SmoothDampAngle(entity.rotation.y, angle, ref myFloat, 0.01f);
-                entity.rotation = new Vector3(0, smooth, 0);
+            Vector3 direction = target.position - entity.position;
 
-                if (direction.magnitude < 2f)
+            // Rotate
+            float myFloat = 0;
+            var angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            var smooth = Mathf.SmoothDampAngle(entity.rotation.y, angle, ref myFloat, 0.01f);
+            entity.rotation = new Vector3(0, smooth, 0);
+
+            if (direction.magnitude < 2)
+            {
+                //  Attack the player
+                switch (entity.GetEntityComponent<AppearanceComponent>().visualId)
                 {
-                    //  Attack the player
-                    switch (entity.GetEntityComponent<AppearanceComponent>().visualId)
-                    {
-                        case "Assets/Art/Characters/Knight/Knight.prefab":
-                            actionManager.TryStartAction(entity, "knight_attack_001");
-                            break;
-                        case "Assets/Art/Characters/Archer/Archer.prefab":
-                            actionManager.TryStartAction(entity, "archer_attack_001");
-                            break;
-                        case "Assets/Art/Characters/Necromancer/Necromancer.prefab":
-                            actionManager.TryStartAction(entity, "necromancer_attack_001");
-                            break;
-                    }
+                    case "Assets/Art/Characters/Knight/Knight.prefab":
+                        actionManager.TryStartAction(entity, "knight_attack_001");
+                        break;
+                    case "Assets/Art/Characters/Archer/Archer.prefab":
+                        actionManager.TryStartAction(entity, "archer_attack_001");
+                        break;
+                    case "Assets/Art/Characters/Necromancer/Necromancer.prefab":
+                        actionManager.TryStartAction(entity, "necromancer_attack_001");
+                        break;
                 }
-                else
-                {
-                    //  Move
-                    var velocity = direction.normalized * entity.GetEntityComponent<CharacterComponent>().masterData.Speed;
-                    entity.velocity = new Vector3(velocity.x, entity.velocity.y, velocity.z);
-                }
+            }
+            else
+            {
+                //  Move
+                var velocity = direction.normalized * entity.GetEntityComponent<CharacterComponent>().masterData.Speed;
+                entity.velocity = new Vector3(velocity.x, entity.velocity.y, velocity.z);
             }
         }
 

624c530d905829bc817a1402e0f95a90103bb08d
Baeinsoo <insoobae@naver.com>
2026-07-20 00:41:55 +0900

    refactor(netcode): GameFramework.Netcode using 추가 (GF 넷코드 수렴 대응)

--- Assets/Scripts/Netcode/MirrorNetworkTime.cs
+++ Assets/Scripts/Netcode/MirrorNetworkTime.cs
@@ -1,9 +1,11 @@
+using GameFramework.Netcode;
+
 namespace LOP
 {
     /// <summary>
     /// 서버 권위 시간을 INetworkTime으로 노출. 서버는 예측이 없어 ServerNow=PredictedTime(둘 다 NetworkTime.time, 일치), Rtt=0.
     /// </summary>
-    public class MirrorNetworkTime : GameFramework.INetworkTime
+    public class MirrorNetworkTime : GameFramework.Netcode.INetworkTime
     {
         public double ServerNow => Mirror.NetworkTime.time;
 

b142b9dba78dd16dcfad4952df5c73b5249fe78c
Baeinsoo <insoobae@naver.com>
2026-08-24 23:07:22 +0900

    refactor(entity): LOPActor가 EntityActor를 상속한다

--- Assets/Scripts/Entity/LOPActor.cs
+++ Assets/Scripts/Entity/LOPActor.cs
@@ -1,14 +1,7 @@
-using UnityEngine;
-
 namespace LOP
 {
-    public class LOPActor : MonoBehaviour
+    /// <summary>서버 쪽 엔티티 몸의 신원. 지금은 <see cref="EntityActor"/>가 전부다.</summary>
+    public class LOPActor : EntityActor
     {
-        public string entityId { get; private set; }
-
-        public void SetEntityId(string entityId)
-        {
-            this.entityId = entityId;
-        }
     }
 }
